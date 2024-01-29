452de081787cbce7915ddfa44611d8c9999b8e31
Stanislau Ryzhkou <stanislaurv@gmail.com>
2024-01-29 19:20:43 +0100

    improved frontend

--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -1,33 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
+import LoginModal from "../../components/loginModal";
 
 const MainPage = () => {
-    const [username, setUsername] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
     return (
-        <div className="w-full h-[100dvh] md:py-10 py-4 bg-opacity-[15%] bg-stone-950 justify-center flex">
-            <div className='flex flex-col p-4 bg-white rounded-lg h-fit'>
-                <h1 className='flex justify-center'>Exchanger</h1>
-                <div className='flex flex-col pt-2'>
-                    <input
-                        className='mb-2 border-2 border-gray-300 rounded-md p-2'
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                        className='mb-2 border-2 border-gray-300 rounded-md p-2'
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button
-                        className='mb-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                        type="submit">Login</button>
-                </div>
-            </div>
+        <div className="w-full h-[100dvh] md:py-10 py-4 bg-opacity-[15%] bg-stone-950 justify-center items-center flex">
+            <h1 className='flex justify-center'>Exchanger</h1>
+            <LoginModal isOpen={isModalOpen} close={
+                () => console.log("close")
+            } />
         </div>
+
     );
 }
 
