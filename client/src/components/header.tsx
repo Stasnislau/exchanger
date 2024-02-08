af811bda24bc3e230da520f10c235df8598acd12
Stanislau Ryzhkou <stanislaurv@gmail.com>
2024-02-08 18:18:40 +0100

    added header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,7 +1,45 @@
+import { useContext } from 'react';
+import { Context } from '../main';
+import { useNavigate } from 'react-router-dom';
+
 const Header = () => {
+    const store = useContext(Context);
+    const navigate = useNavigate();
     return (
-        <header>
-            <h1>My App</h1>
+        // Use the Tailwind CSS classes to style your header similarly to the image
+        <header className="bg-gradient-to-t from-[#0fb2fc] from-70% inset-0 to-100% mix-blend-screen to-[#10cffc] md:h-14 flex"
+            style={
+                {
+                    boxShadow: '0 0 3px rgba(0, 0, 0), 0 0 10px rgba(36, 93, 176, 0.8), 0 0 30px rgba(36, 93, 176, 0.8),  0 0 40px rgba(255, 255, 255, 0.6)'
+
+                }
+            }
+        >
+            <div className="container mx-4 flex grow justify-between items-center ">
+                <div className="flex items-center">
+                    <img src="src/assets/images/euro.png" alt="euro" className="h-10 m-0 rounded-full" style={{
+                        boxShadow: '0 0 10px rgba(36, 93, 176, 0.8), 0 0 30px rgba(36, 93, 176, 0.8),  0 0 40px rgba(255, 255, 255, 0.6)'
+                    }} />
+                </div>
+                <div className="flex items-center">
+                    <h1 className="text-xl font-bold">Currency Exchange</h1>
+                </div>
+                <div className="flex items-center flex-row gap-4">
+                    {store.state.isLoggedIn ? <button
+                        className='p-0 m-0 border-none transition-all duration-300 ease-in-out hover:text-secondary'
+                        onClick={() => console.log("HISTORY")}>History</button> : null}
+                    {store.state.isLoggedIn ? <button
+                        className='p-0 m-0 border-none transition-all duration-300 ease-in-out hover:text-secondary'
+                        onClick={async () => {
+                            await store.logout()
+                            navigate('/login')
+                        }}>Logout</button> : null}
+                    {!store.state.isLoggedIn
+                        ? <button
+                            className='p-0 m-0 border-none transition-all duration-300 ease-in-out hover:text-secondary'
+                            onClick={() => navigate("/login")}>Login</button> : null}
+                </div>
+            </div>
         </header>
     );
 }
--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -1,10 +1,12 @@
+import { Header } from "../../components";
 
 const MainPage = () => {
 
     return (
-        <div className="w-full h-[100dvh] md:py-10 py-4 bg-opacity-[15%] bg-stone-950 justify-center items-center flex">
-            <h1 className='flex justify-center'>Exchanger</h1>
-            
+        <div>
+            <Header />
+            <h1>Home</h1>
+            <p>This is the home page</p>
         </div>
 
     );
